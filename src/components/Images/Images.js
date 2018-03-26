94625cf3336267ef3c67d0f4346a9af96cf26768
siddharthkul <siddharthkul1995@gmail.com>
2018-03-26 15:54:44 -0700

    Converted images to statefull component and lifecycle methods added

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 
-const Images = () => (
-	<h2>Images</h2>
-)
+class Images extends Component {
+    constructor(props){
+        super(props);
+        console.log(props.location.state)
+    }
+    componentDidMount(){
+        console.log('Loading API data for Images');
+    }
+    componentWillUnmount(){
+        console.log('Unmounting Web for Images');
+    }
+    render(){
+        //console.log(this.props.location.state);
+        return(
+                <h2>Images</h2>
+        );
+    }
+}
 
 export default Images;
